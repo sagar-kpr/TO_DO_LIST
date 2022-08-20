06a0d1fab22859e5a4994c82564c2789b9f582df
sagar-kpr <102784500+sagar-kpr@users.noreply.github.com>
2022-08-20 18:24:32 +0530

    local storage

--- index.js
+++ index.js
@@ -15,7 +15,163 @@ var count = document.getElementById("count");
 var val;
 var tasks = [];
 var li;
+var element = [];
 
+
+window.onload = function() {
+  //localStorage.removeItem('todo');
+  let tasklistItem;
+  let task=[];
+  if(localStorage.getItem('todo') == null) {
+    return;
+  }
+  element =  JSON.parse(localStorage.getItem('todo'))
+  for(let j =0 ; j<element.length; j++){
+    tasklistItem = document.createElement('li');
+    let date = j;
+    tasklistItem.setAttribute("id", date);
+    tasklistItem.innerHTML = `<div class="test"> <div class="icon" id=${date} data-value = false 
+    style="background: ${colors[Math.floor(Math.random() * colors.length)]};">
+   <i id=${date} data-value = false class="fa-solid fa-circle-notch" ></i>
+   <div class="tick-mark" id=${date} data-value = false></div>
+   </div>
+   <div class="tooltipbox" id=${date} data-value = false ><small><strong>Mark As Complete</strong></small></div>
+   <div class="tooltip" id=${date} data-value = false ><span class="span" data-value = false id=${date}> ${element[j]} </span></div>
+   <i id=${date} data-value = false class="fa-solid fa-trash-can del" ></i></div>`;
+  
+  tasklist.appendChild(tasklistItem);
+  task.push(tasklistItem);    
+  }
+  count.innerHTML = JSON.parse(localStorage.getItem('count'));
+  input.value = "";
+  console.log("1111",task);
+
+  function handlemouse(e){
+    let mouse = e.target;
+    if (
+      mouse.className == "fa-solid fa-circle-notch" ||
+      mouse.className == "icon"
+    ) {
+      let tooltip = document.querySelectorAll(".tooltipbox");
+      for (let i = 0; i < element.length; i++) {
+        let val = tooltip[i].getAttribute("data-value");
+        if (tooltip[i].id == mouse.id && val == "false") {
+          tooltip[i].style.display = "block";
+          return;
+        } else if (tooltip[i].id == mouse.id && val == true) {
+          tooltip[i].style.display = "none";
+          return;
+        }
+      }
+    }
+  }
+
+  function handleleave(e) {
+    let mousel = e.target;
+    if (
+      mousel.className == "fa-solid fa-circle-notch" ||
+      mousel.className == "icon"
+    ) {
+      let tooltip = document.querySelectorAll(".tooltipbox");
+  
+      for (let i = 0; i < element.length; i++) {
+        if (tooltip[i].id == mousel.id) {
+          tooltip[i].style.display = "none";
+          return;
+        }
+      }
+    }
+  }
+
+  function handleclick(e) {
+    const trgt = e.target;
+    if (trgt.className == "fa-solid fa-trash-can del") {
+      const deleteid = trgt.id;
+      console.log("to be del",deleteid);
+      remove(deleteid);
+      return;
+    } else if (
+      trgt.className == "icon" ||
+      trgt.className == "fa-solid fa-circle-notch" ||
+      trgt.className == "tick-mark"
+    ) {
+      const tick = trgt.id;
+      checkbox(tick);
+      return;
+    }
+  }
+
+  function addtodom(task) {
+    for (let i = 0; i < task.length; i++) {
+      tasklistItem.innerHTML = task[i].innerHTML
+      tasklist.appendChild(tasklistItem);
+    }
+  }
+
+  function remove(deleteid) {
+    for (let i = 0; i < element.length; i++) {
+      
+      if (deleteid == task[i].id) {
+        let todo = JSON.parse(localStorage.getItem('todo'));
+        
+        for(let k=0; k< todo.length; k++){
+          if(task[i].innerText == todo[k] ){
+
+            console.log('before',JSON.parse(localStorage.getItem('todo')));
+            todo.splice(k,1);
+            localStorage.clear();
+            localStorage.setItem('todo', JSON.stringify(todo));
+            console.log('after',JSON.parse(localStorage.getItem('todo')));
+            addtodom(task);
+  
+          }
+        }
+        count.innerHTML = --j; 
+      
+        return;
+      }
+    }
+  }
+
+  function checkbox(tick) {
+    for (let i = 0; i < task.length; i++) {
+      if (tick == task[i].id) {
+        let findtick = document.querySelectorAll(".tick-mark");
+        let findspan = document.querySelectorAll(".span");
+        let findicon = document.querySelectorAll(".icon");
+        let findtooltip = document.querySelectorAll(".tooltipbox");
+        for (let j = 0; j < findtick.length; j++) {
+          if (tick == findtick[j].id && tick == findspan[j].id) {
+            let val = findspan[j].getAttribute("data-value");
+  
+            if (val == "false") {
+              findtick[j].style.display = "block";
+              findspan[j].style.textDecoration = "line-through";
+              findspan[j].style.textDecorationThickness = "2px";
+              findicon[j].style.boxShadow =
+                "0px 0px 20px 8px rgba(255,255,255,0.7) inset";
+              findspan[j].setAttribute("data-value", true);
+              findtooltip[j].setAttribute("data-value", true);
+              return;
+            }
+            findtick[j].style.display = "none";
+            findspan[j].style.textDecoration = "none";
+            findicon[j].style.boxShadow = "none";
+            findspan[j].setAttribute("data-value", false);
+            findtooltip[j].setAttribute("data-value", false);
+            return;
+          }
+        }
+      }
+    }
+  }
+
+
+  document.addEventListener("mouseover", handlemouse);
+  document.addEventListener("mouseout", handleleave);
+  document.addEventListener("click", handleclick);
+    
+}
 
 //IT WILL ADD THE TASK THROUGH 'ADD' BUTTON
 btn.addEventListener("click", function () {
@@ -26,20 +182,22 @@ btn.addEventListener("click", function () {
     li = document.createElement("li");
     let date = Date.now();
     li.setAttribute("id", date);
-
     li.innerHTML = `<div class="test"> <div class="icon" id=${date} data-value = false 
      style="background: ${colors[Math.floor(Math.random() * colors.length)]};">
     <i id=${date} data-value = false class="fa-solid fa-circle-notch" ></i>
     <div class="tick-mark" id=${date} data-value = false></div>
     </div>
-
-      <div class="tooltipbox" id=${date} data-value = false ><small><strong>Mark As Complete</strong></small></div>
-      <div class="tooltip" id=${date} data-value = false ><span class="span" data-value = false id=${date}> ${val} </span></div>
-      <i id=${date} data-value = false class="fa-solid fa-trash-can del" ></i></div>`;
+    <div class="tooltipbox" id=${date} data-value = false ><small><strong>Mark As Complete</strong></small></div>
+    <div class="tooltip" id=${date} data-value = false ><span class="span" data-value = false id=${date}> ${val} </span></div>
+    <i id=${date} data-value = false class="fa-solid fa-trash-can del" ></i></div>`;
     tasklist.append(li);
     tasks.push(li);
     input.value = "";
-    count.innerHTML = ++j;
+    element.push(li.innerText);
+    console.log(element);
+    localStorage.setItem('todo', JSON.stringify(element))
+    //localStorage.setItem('count',JSON.stringify(count.innerHTML) );
+    
   }
 });
 
@@ -70,6 +228,9 @@ function handler(e) {
       tasklist.append(li);
       tasks.push(li);
       input.value = "";
+      element.push(li.innerText);
+      console.log(element);
+      localStorage.setItem('todo', JSON.stringify(element))
       let count = document.getElementById("count");
       count.innerHTML = ++j;
     }
@@ -88,9 +249,11 @@ function addtodom(tasks) {
 function remove(deleteid) {
   for (let i = 0; i < tasks.length; i++) {
     if (deleteid == tasks[i].id) {
+      console.log("000",tasks[i]);
       tasks.splice(i, 1);
       count.innerHTML = --j;
       addtodom(tasks);
+
       return;
     }
   }
@@ -160,7 +323,6 @@ function handlemouse(e) {
 
     for (let i = 0; i < tasks.length; i++) {
       let val = tooltip[i].getAttribute("data-value");
-      console.log(val);
       if (tooltip[i].id == mouse.id && val == "false") {
         tooltip[i].style.display = "block";
         return;
@@ -184,7 +346,6 @@ function handleleave(e) {
     for (let i = 0; i < tasks.length; i++) {
       if (tooltip[i].id == mousel.id) {
         tooltip[i].style.display = "none";
-        console.log(tooltip[i]);
         return;
       }
     }
